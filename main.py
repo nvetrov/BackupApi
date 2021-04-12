bf3f3d823068aa379280fc3dc413f9096e1a28c8
nvetrov <nvetrov@gmail.com>
2021-04-12 23:35:37 +0300

    5 == 5?

--- main.py
+++ main.py
@@ -1,47 +1,3 @@
-#  https://github.com/netology-code/py-diplom-basic
-#  Нужно написать программу для резервного копирования фотографий с профиля(аватарок) пользователя vk в облачное хранилище
-#  Яндекс.Диск.
-#  Для названий фотографий использовать количество лайков, если количество лайков одинаково, то добавить дату загрузки.
-#  Информацию по сохраненным фотографиям сохранить в json-файл.
-#  ------------------Задание по пунктам -----------------------------------------------------------
-#  1. Получать фотографии с профиля. Для этого нужно использовать метод  photos.get(https://vk.com/dev/photos.get).
-#  2. Сохранять фотографии максимального размера(ширина/высота в пикселях) на Я.Диске.
-#  3. Для имени фотографий использовать количество лайков.
-#  4. Сохранять информацию по фотографиям в json-файл с результатами.
-
-'''
- Входные данные
-
-Пользователь вводит:
-
-1. id пользователя vk;
-2. токен с Полигона Яндекс.Диска.
-Важно: Токен публиковать в github не нужно!
-
-# Выходные данные:
-
-1. json-файл с информацией по файлу:
-    [{
-    "file_name": "34.jpg",
-    "size": "z"
-    }]
-
-2.Измененный Я.диск, куда добавились фотографии.'''
-
-# Обязательные требования к программе:
-# 1. Использовать REST API Я.Диска и ключ, полученный с полигона.
-# 2. Для загруженных фотографий нужно создать свою папку.
-# 3. Сохранять указанное количество фотографий(по умолчанию 5) наибольшего размера (ширина/высота в пикселях) на Я.Диске
-# 4. Сделать прогресс-бар или логирование для отслеживания процесса программы.
-# 5. Код программы должен удовлетворять PEP8.
-
-# Необязательные требования к программе:
-
-# 1. Сохранять фотографии и из других альбомов.
-# 2. Сохранять фотографии из других социальных сетей. Одноклассники и Инстаграмм
-# 3. Сохранять фотографии на Google.Drive.
-
-
 import copy
 import json
 import os
@@ -71,9 +27,9 @@ class YaUploader:
         }
         response = requests.put(_url, headers=headers, params=params)
         if response.status_code != 201:
-            print(f'Папка с именем: {self.y_directory} уже существует!')
+            print(f'Folder named: {self.y_directory} already exists!')
         else:
-            print(f'Папка: {self.y_directory} создана на Yandex disc')
+            print(f'Folder: {self.y_directory} created on Yandex Disk')
 
     # Загружать файл на яндекс Диск.
     def upload_file_y(self, file_path: str, url):
@@ -90,9 +46,7 @@ class YaUploader:
         for link in photo_lists:
             if 'url' in link and 'file_name' in link:
                 uploader.upload_file_y(ya_folder + '/' + link['file_name'], link['url'])
-        return True
-
-
+        return len(photo_lists)
 
 
 #  класс VK по работе с api
@@ -101,27 +55,28 @@ class VK:
         self.token = token
         self.vk = _vk_id  # id пользователя vk
         self.v = version  # версия api vk
-
         self.count = count  # количество фоток по умолчанию 5
 
-    #     Получить фотографии с профиля vk.
+    # Получить фотографии с профиля vk.
     def connect_vk(self):
         link_vk = 'https://api.vk.com/method/photos.get'
         params = {'owner_id': self.vk,
                   'access_token': self.token,
                   'album_id': 'profile',
                   'photo_sizes': 1,
-                  'count': 5,
+                  'count': self.count,
                   'extended': 1,
                   'feed_type': "likes",
                   'v': self.v,
                   }
+        print(f'self.count: {self.count}')
         photos_vk = requests.get(link_vk, params=params)
         if photos_vk.status_code != 200:
-            print('Ошибка')
+            print('No != 200')
         else:
             return photos_vk.json()
 
+    # Сохранить лог
     def save_json_log_vk(self, dict_data):
         #  Создаем папку с названием альбома
         # os.rmdir('saved')
@@ -135,83 +90,52 @@ class VK:
         # Добавляю путь к локальный файлам.
         prep_dict = {'path_local': photo_folder}
         dict_data.append(prep_dict)
-        # Копируем данные
+        # Копируем данные для передачи url, path_local
         urls_dict = copy.deepcopy(dict_data)
-        # Убираем url, file_name_real_name из выходных данных:
+        # Убираем url, path_local из выходных данных .json:
         for i in dict_data:
-            if 'url' in i and \
-                    'file_name_real_name' in i:
-                del i["url"]
-                del i["file_name_real_name"]
+            if 'url' in i:
+                del i['url']
+            elif 'path_local' in i:
+                del i["path_local"]
         # Сохраняем в лог файл на диск
-        with open('photos_log.json', 'w') as file:
+        with open(f'{self.vk}.json', 'w') as file:
             json.dump(dict_data, file, indent=2, ensure_ascii=False)
 
         return urls_dict
 
     def parse_profile_vk(self, answer):
-        count = 1
+        count = 0
         # Обработка ответа от VK
         ll = answer['response']['items']
-
         if len(ll) == 0:
             print(f'Photo not found in profile id{self.vk})')
             exit()
-
+        print(ll)
         photo_lst = []
-        print("Download from VK. Please wait...")
         for item in ll:
             if count < self.count:
                 photo_dict = dict()
                 photo_dict['file_name'] = str(item['likes']['count']) + '.jpg'
                 photo_dict['size'] = item['sizes'][-1]['type']
                 photo_dict['url'] = item['sizes'][-1]['url']
-
-                # print(count, photo_dict)
-
-                # выбираем тип фото z
-                # for i in item['sizes']:
-                #     if i['type'] == 'z':
-                #         # Формируем название файла фото по кол-ву лайков
-                #         photo_dict['file_name'] = str(item['likes']['count']) + '.jpg'
-                #         photo_dict['size'] = i['type']
-                #         #  Показывать пользователю в консоли для лога
-                #         photo_dict['file_name_real_name'] = os.path.basename(urlparse(i['url']).path)
-                #         photo_dict['url'] = i['url']
-                #         print(
-                #             f'{count}, photo {photo_dict["file_name_real_name"]}, from {photo_dict["url"]}, | {i["type"]}')
-                #         count += 1
-                #     else:
-                #         photo_dict['file_name'] = str(item['likes']['count']) + '.jpg'
-                #         photo_dict['size'] = item['sizes'][-1]['type']
-                #         #  Показывать пользователю в консоли для лога
-                #         photo_dict['file_name_real_name'] = os.path.basename(urlparse(i['url']).path)
-                #         photo_dict['url'] = i['url']
-                #         print(
-                #             f'{count}, photo {photo_dict["file_name_real_name"]}, from {photo_dict["url"]}, | {i["type"]}')
-
-                count += 1
-
+                print('Загрузка:', item['sizes'][-1]['url'])
+                # print(photo_dict['file_name'], photo_dict['size'], photo_dict['url'])
                 photo_lst.append(photo_dict)
-
-        # pprint(photo_lst)
-        print(f'Downloaded from VK profile.\n'
-              f'For more information see  "photos_log.json"')
+                count += 1
         return photo_lst
 
 
 if __name__ == '__main__':
     print('input data and press enter'.upper())
-    TOKEN_Y = 'AgAAAAAAS0ctAADLW5s95K1kRERdr_-hgG5KTnI'  # input(f"Input token from Yandex.Disk Polygon: ")
+    TOKEN_Y = 'AgAAAAAAS0ctAADLW5s95K1kRERdr_-hgG5KTnI'  # input(f"Input token(Yandex.Disk Polygon): ")
 
-    vk_id = 33636542  # int(input("Input vk user id: "))
-    # vk_id = 552934290  # int(input("Input vk user id: "))
-    # vk_id = 2419874  # 552934290 #int(input("Input vk user id: "))
-    vk_token = '230860474a5a4588be7af906b5fc4d3b038401bbeb578b20603e956ab02b971c41fb78c9f1f8b02b887f4'  # str(input(f"Input token VK: "))
+    vk_id = 552934290 # int(input("Input vk user id: "))
+    vk_token = "230860474a5a4588be7af906b5fc4d3b038401bbeb578b20603e956ab02b971c41fb78c9f1f8b02b887f4" #str(input(f"Input token VK: "))
     print('--------------------------------------------')
     vk_v = 5.89
     # vk_v = 5.130   #  Current version
-    count_photos = 5  # Количество скаченных фото по умолчанию
+    count_photos = 10 # Количество скаченных фото по умолчанию
 
     # Инициализация класс VK -> v_obj
     v_obj = VK(_vk_id=vk_id, token=vk_token, version=vk_v, count=count_photos)
@@ -219,26 +143,17 @@ if __name__ == '__main__':
     # Получили данные из профиля VK пользователя
     r = v_obj.connect_vk()
 
-    # Логирование + обработка
+    # Логирование
     full_logs = v_obj.parse_profile_vk(r)
-
-    ##  Не нужно на компьютер сохранять фотографии. Достаточно файл с логом.
-    ##Сохранить фото на ПК оффлайн.
-    # photo_links_vk = v_obj.save_json_log_vk(dict_data=full_logs)
-    # for p in photo_links_vk:
-    #     if 'url' in p:
-    #         rr = requests.get(p['url'])
-    #         with open(p['file_name'], 'wb') as f:
-    #             f.write(rr.content)
-    #     else:
-    #         continue
-    ## Сохранить фото на ПК офлайн.
-
     photo_links_vk = v_obj.save_json_log_vk(dict_data=full_logs)
-    print('')
-    # Сохраняем фото на yandex disk в папку по названию альбому
+
+    # Название альбома на ЯДиске
     ya_folder = photo_links_vk[::-1][0]['path_local'].split('/')[1]
+
     uploader = YaUploader(token=TOKEN_Y, ya_folder=ya_folder)
-    print(f"Total files: {len(photo_links_vk) - 1}.  ")
-    uploader.get_url_y(photo_lists=photo_links_vk)
+
+    # Сохранение фото на Y.Диск
+    c = uploader.get_url_y(photo_lists=photo_links_vk)
+
     print(f'Upload completed successfully to directory "{ya_folder}".')
+    print(f'Saved photos:{c - 1}')
